e8edbb6621ac08c464c6178501f44906245f4bb7
David Onche <daveonche@gmail.com>
2020-08-02 21:03:49 +0100

    Updated Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import Card from './Card';
 
-const CardList = ({ robots }) => {
+const Card = ({ name, email, id }) => {
     return (
-        <div>
-            {
-                robots.map((user, i) => {
-                    return (
-                        <Card
-                            key={i}
-                            id={robots[i].id}
-                            name={robots[i].name}
-                            email={robots[i].email}
-                        />
-                    );
-                })
-            }
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt='robot' src={`https://robohash.org/${id}?200x200`} />
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
         </div>
     );
 }
 
-export default CardList;
+export default Card;
